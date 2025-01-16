e1ea17f13bc2717e0434de6aab4356092e3bca98
eya-harbaoui <eyaharbaoui651@gmail.com>
2025-01-16 15:45:04 +0100

    next js app + shadcn setup

---
+++ rag-next-app/app/page.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex flex-col min-h-screen items-center p-24">User's landing page</main>
+  );
+}
---
+++ rag-next-app/app/pinecone/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const VectorDBPage = (props: Props) => {
+  return (
+    <main className='flex flex-col items-center'></main>
+  )
+}
+
+export default VectorDBPage;
